1e20a21e1faf924cecce0ad883d8c29827cbe6be
Bruno Paula <brunopaula@dryserv.com>
2020-06-17 14:38:48 -0400

    added a working timer component

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import Timer from 'react-compound-timer'
 
-function Timer() {
-    return <div className="timer">00:03</div>
+
+
+function GameTimer() {
+
+const timerBtn={
+    padding: "5px 20px", 
+    margin: '0 20px', 
+    borderRadius: '10px',
+    outline: 'none'
 }
 
-export default Timer
+    return (
+    <div className="timer">
+        <Timer initialTime={900000} direction="backward">{({resume, pause}) => (
+            <>
+                <Timer.Minutes/> : <Timer.Seconds />
+                <div style={{fontFamily: "arial", padding: "0 80px"}}>
+                    <button style={timerBtn} onClick={resume}>Resume</button>
+                    <button style={timerBtn} onClick={pause}>Pause</button>
+                </div>
+            </>)}
+        </Timer>
+    </div>)
+}
+
+export default GameTimer
